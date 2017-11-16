618b0f9eb1bbd4ec8228ed48f1ff48fa150d2ea3
The Man <doit@keemail.me>
2017-11-17 05:11:04 +0700

    fixed padding for small screen

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from './Filters.css'
 
 const Filters = () => (
-  <ul>
-    <li>All</li>
-    <li>Important</li>
-    <li>Very important</li>
-    <li>Search: _</li>
-  </ul>
+  <div className={styles.filters}>
+    <span className={styles.filterLinks}>
+      <a>All</a>
+      <a>Important</a>
+      <a>Important</a>
+      <a>Very important</a>
+    </span>
+    <span className={styles.search}>Search</span>
+  </div>
 )
 
 Filters.propTypes = {}
