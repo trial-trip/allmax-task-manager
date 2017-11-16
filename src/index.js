5d52f06434327ca42f5ee55882d8cb3537dd0d93
The Man <doit@keemail.me>
2017-11-16 15:19:31 +0700

    added react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+// react
+import React from 'react'
+import { render } from 'react-dom'
+
 const rootElement = document.createElement("div")
 document.body.appendChild(rootElement)
+
+render(
+    <p>hello world</p>,
+  rootElement
+)
