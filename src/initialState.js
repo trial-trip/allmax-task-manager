79836ec800a92e8d925b156f5d3b5b679667abd0
The Man <doit@keemail.me>
2017-11-18 17:13:51 +0700

    new Dates generator

--- src/initialState.js
+++ src/initialState.js
@@ -10,6 +10,10 @@ try {
 const randomBool = () => Math.random() < .5
 const randomPriority = () => Math.floor(Math.random() * 3)
 
+const yearDuration = (365 * 24 * 3600 * 1000)
+const startingDate = Date.parse('2017.09.01')
+const randomDate = () => Math.floor(Math.random() * yearDuration) + startingDate  
+
 const stateDemo = {
   priorityFilter: 'SHOW_ALL',
   tasks: Array(17)
@@ -18,12 +22,12 @@ const stateDemo = {
       title: 'Run 10 km',
       description: 'That will make you glad...\n\nMaybe.',
       priority: 1,
-      deadline: '2014-11-25T03:13:37.000Z',
-      completionDate: '2016-11-16T03:13:37.000Z'
+      deadline: 'DATE_UNSET',
+      completionDate: 'DATE_UNSET'
     })
     .map((item, i) => ({ ...item, id: i }))
-    .map((item, i) => ((randomBool()) ? { ...item, completionDate: '' } : item))
-    .map((item, i) => ((randomBool()) ? { ...item, deadline: '' } : item))
+    .map((item, i) => ({ ...item, completionDate: randomBool() ? randomDate() : 'DATE_UNSET' }))
+    .map((item, i) => ({ ...item, deadline: randomBool() ? randomDate() : 'DATE_UNSET' }))
     .map((item, i) => ((randomBool()) ? { ...item, description: '' } : item))
     .map((item, i) => ({ ...item, priority: randomPriority() }))
 }
