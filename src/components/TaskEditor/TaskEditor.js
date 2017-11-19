e92e5f3dd7cae3db64feb9ce3bda2906ff827d0e
The Man <doit@keemail.me>
2017-11-19 22:52:34 +0700

    priority editing made the right way

--- src/components/TaskEditor/TaskEditor.js
+++ src/components/TaskEditor/TaskEditor.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import styles from './TaskEditor.css'
+import btn from '../../util/btn.css'
 
 class TaskEditor extends React.Component {
   constructor(props) {
     super(props)
+
     const task = props.tasks.find(t => t.id === +props.match.params.id)
 
     if (task) {
       this.state = {
+        type: 'EDIT_TASK',
         id: task.id,
         title: task.title,
         description: task.description,
-        priority: task.priority,
-        deadline: task.deadline,
+        priority: `${task.priority}`,
+        deadline: +task.deadline || undefined,
       }
     } else {
       this.state = {
+        type: 'ADD_TASK',
         id: props.newTaskId,
         title: '',
         description: '',
-        priority: 0,
+        priority: "0",
         deadline: 'DATE_UNSET',
       }
     }
@@ -29,8 +34,8 @@ class TaskEditor extends React.Component {
 
   handleChange(event) {
     const target = event.target
+    console.log(target.name, target.value)
     this.setState({
-      status: '',
       [target.name]: target.value
     })
   }
@@ -42,42 +47,54 @@ class TaskEditor extends React.Component {
   }
 
   render() {
+    console.log(this.state.priority)
     return (
       <form onSubmit={this.handleSubmit} >
-        <div>
+        <div className={styles.block}>
           <input 
+            required
             type="text" 
             name="title" 
-            placeholder="title" 
+            placeholder="Task title*" 
             value={this.state.title} 
+            className={styles.in}
             onChange={this.handleChange} />
         </div>
-        <div>
+        <div className={styles.block}>
           <textarea 
             name="description" 
-            placeholder="description" 
+            placeholder="Detailed description" 
             value={this.state.description} 
+            className={styles.in}
             onChange={this.handleChange} />
         </div>
-        <div>
-          <input 
-            type="text" 
-            name="priority" 
-            placeholder="priority" 
-            value={this.state.priority} 
-            onChange={this.handleChange} />
+        <div className={styles.block}>
+          <label >
+            <input type="checkbox" name="priority" value="0" checked={this.state.priority === "0"} onChange={this.handleChange} />
+            <span>Ordinary</span>
+          </label>
+          <label>
+            <input type="checkbox" name="priority" value="1" checked={this.state.priority === "1"} onChange={this.handleChange} />
+            <span className={styles.important}>Important</span>
+          </label>
+          <label>
+            <input type="checkbox" name="priority" value="2" checked={this.state.priority === "2"} onChange={this.handleChange} />
+            <span className={styles.veryImportant}>Very important</span>
+          </label>
         </div>
-        <div>
+        <div className={styles.block}>
           <input 
             type="date" 
             name="deadline" 
             placeholder="deadline" 
             value={this.state.deadline} 
+            className={styles.in}
             onChange={this.handleChange} />
         </div>
-        <div>
+        <div className={styles.block}>
           <input 
             type="submit" 
+            className={btn.btn}
             value="Save" />
         </div>
       </form>
