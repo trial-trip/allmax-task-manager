9de840d99ebc49669db49f1659836cad7121f53c
The Man <doit@keemail.me>
2017-11-20 00:18:52 +0700

    fixed synrax error

--- src/util/initialState.js
+++ src/util/initialState.js
@@ -3,7 +3,7 @@ let stateOption
 try {
   const str = localStorage.getItem('tm_data');
   stateOption = JSON.parse(str);
-} catch () {
+} catch (e) {
 }
 
 // Alternative solution to have state is to populate state with demo data.
