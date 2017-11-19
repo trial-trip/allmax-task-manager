68623e9796d50828ce13ea2f624c79c44c2b90c6
The Man <doit@keemail.me>
2017-11-20 05:17:53 +0700

    shame, yes

--- src/components/FilterList/FilterList.spec.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-import React from 'react'
-import { shallow, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-import FilterList from './FilterList'
-
-configure({ adapter: new Adapter() })
